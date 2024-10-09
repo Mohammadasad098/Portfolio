bb139f993af549477b5007478a4557b07ee4c4cc
Mohammadasad098 <mohammadasadiam64@gmail.com>
2024-10-10 04:51:29 +0500

    work is ongoing

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { auth , sendData } from '../config/firebase/firebasemethods';
+import { onAuthStateChanged } from 'firebase/auth';
 
 const Contact = () => {
+
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const [blogs, setBlogs] = useState([]);
+  const sendDatatoFirestore = async (data) => {
+    setLoading(true);
+    try {
+      const response = await sendData({
+        title: data.title,
+        description: data.description,
+        uid: auth.currentUser.uid,
+      }, 'blogs');
+
+      setBlogs((prevBlogs) => [
+        ...prevBlogs,
+        {
+          title: data.title,
+          description: data.description,
+          uid: auth.currentUser.uid,
+        },
+      ]);
+      reset();
+      // console.log(response);
+    } catch (error) {
+      // console.error(error);
+
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
   return (
     <div className='bg-[#3189ac] py-[220px]'>
       <p className='text-center'><span className='text-5xl font-semibold'>GET IN TOUCH</span> <br />
