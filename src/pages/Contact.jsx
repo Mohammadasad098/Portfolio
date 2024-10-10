6840b80cd7bd79a969e15a768e195232e6ab4191
Mohammadasad098 <mohammadasadiam64@gmail.com>
2024-10-10 05:46:35 +0500

    work is ongoing

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,46 +1,23 @@
-import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { auth , sendData } from '../config/firebase/firebasemethods';
-import { onAuthStateChanged } from 'firebase/auth';
+
 
 const Contact = () => {
 
   const { register, handleSubmit, reset, formState: { errors } } = useForm();
-  const [blogs, setBlogs] = useState([]);
-  const sendDatatoFirestore = async (data) => {
-    setLoading(true);
-    try {
-      const response = await sendData({
-        title: data.title,
-        description: data.description,
-        uid: auth.currentUser.uid,
-      }, 'blogs');
-
-      setBlogs((prevBlogs) => [
-        ...prevBlogs,
-        {
-          title: data.title,
-          description: data.description,
-          uid: auth.currentUser.uid,
-        },
-      ]);
-      reset();
-      // console.log(response);
-    } catch (error) {
-      // console.error(error);
-
-    } finally {
-      setLoading(false);
-    }
-  };
-
-
+  const onSubmit = (data) => console.log(data)
+ 
   return (
     <div className='bg-[#3189ac] py-[220px]'>
       <p className='text-center'><span className='text-5xl font-semibold'>GET IN TOUCH</span> <br />
       <span className='text-[19px]'>I would like to know the final requirements of your project. Please get in touch with me and share details about your project.</span></p>
       <div>
-        
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("name")}  type="text" placeholder='Name' />
+          <input {...register("email")} type="email" placeholder='Email' />
+          <textarea  rows="4" {...register("description")}
+          className="textarea textarea-bordered w-full border-2 p-2"></textarea>
+          <button type='submit'>submit</button>
+        </form>
       </div>
 
     </div>
