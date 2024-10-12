12816cf401d01e5e09b59ce9ad5bfc8122d5e1a4
Mohammadasad098 <mohammadasadiam64@gmail.com>
2024-10-13 02:45:37 +0500

    portfolio completed

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 const Experience = () => {
   return (
-    <div className='bg-[#3189ac] py-[220px]'>
-      <div className='flex justify-evenly items-center'>
-      <div>
-<p className='text-white font-semibold text-4xl'>I am studying web development at SMIT.</p>
+    <div className='bg-[#3189ac] py-[280px]'>
+      <div className='flex flex-col md:flex-row justify-center items-center flex-wrap md:justify-evenly'>
+        <div className='mb-8 md:mb-0'> {/* Increased margin-bottom for more gap */}
+          <p className='text-white text-center font-semibold text-4xl'>I am studying web development at SMIT.</p>
+        </div>
+        <div>
+          <img 
+            width={300} // Reduced image width for mobile screens
+            src="/images/experience.webp" 
+            alt="Experience" 
+            className='md:w-[400px]' // Keeps original width for larger screens
+          />
+        </div>
       </div>
-      <div>
-        <img width={400} src="/images/experience.webp" alt="" />
-      </div>
-      </div>
-      
     </div>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
+
